12ba193572a782546f2f57ea92b5aefebdbe1f75
Vladimir Mitin <vmitin.dev@gmail.com>
2019-12-22 22:08:35 +0300

    added underline for active nav

--- front/src/components/nav.tsx
+++ front/src/components/nav.tsx
@@ -1,24 +1,32 @@
 import React from 'react'
 import { observer } from 'mobx-react'
+import classNames from 'classnames'
 import { useStores } from '../shared-state/contexts'
 import { Routes } from '../routes'
 
-import { Link } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
 import './nav.css'
 
 export const Nav: React.FC<{}> = observer(() => {
     const { loginStore } = useStores()
+    const { pathname } = useLocation()
 
     return (
         <>
             <ul className="nav">
-                <li>
+                <li className={classNames({
+                    'active': pathname === Routes.ROOT
+                })}>
                     <Link to={Routes.ROOT}>Home</Link>
                 </li>
-                <li className="second-last-item">
+                <li className={classNames('second-last-item', {
+                    'active': pathname === Routes.ADD_TRANSACTIONS
+                })}>
                     <Link to={Routes.ADD_TRANSACTIONS}>Add-transactions</Link>
                 </li>
-                <li>
+                <li className={classNames({
+                    'active': pathname === Routes.LOGIN
+                })}>
                     { loginStore.isAuthenticated ? (
                         <Link
                             to={Routes.LOGIN}
